219e488bde5d1c4b9c36830e3473c167b7753772
Anar.N <an301@duke.edu>
2026-04-27 14:20:01 +0800

    Add HackDKU 2026 recap; convert all blog images to webp

--- src/components/teambanner.tsx
+++ src/components/teambanner.tsx
@@ -1,26 +1,25 @@
-// components/TeamBanner.js
-
 import Image from "next/image";
 
 export function TeamBanner() {
-  return (
-    <div className=" relative w-screen h-[200px] sm:h-[300px] md:h-[390px] shadow-md">
-      <Image
-        // Ensure this image exists in the public directory
-        src="/team-banner.png"
-        alt="Our Team"
-        className="opacity-60"
-        priority
-        fill
-        style={{
-          objectFit: "cover"
-        }} />
-      {/* Overlay Container */}
-      <div className="absolute inset-0 bg-opacity-50 flex items-center justify-center">
-      <h1 className=" object-cover font-lexend object-covertext-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-white opacity-90 text-center px-4">
-        Coding the Future Together
-      </h1>
-      </div>
-    </div>
-  );
+	return (
+		<div className=" relative w-screen h-[200px] sm:h-[300px] md:h-[35vw] shadow-md">
+			<Image
+				// Ensure this image exists in the public directory
+				src="/banner.webp"
+				alt="Our Team"
+				className="opacity-70"
+				priority
+				fill
+				style={{
+					objectFit: "cover",
+				}}
+			/>
+			{/* Overlay Container */}
+			<div className="absolute inset-0 bg-opacity-50 flex items-center justify-center">
+				<h1 className=" object-cover font-lexend object-covertext-2xl text-3xl sm:text-4xl md:text-5xl drop-shadow-md lg:text-7xl font-extralight text-white opacity-90 text-center px-4">
+					Coding the Future Together
+				</h1>
+			</div>
+		</div>
+	);
 }
