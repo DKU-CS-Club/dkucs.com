9e1abc1b24e87bc0db65190a2e7140044971fc8a
Anar.N <an301@duke.edu>
2026-04-27 10:38:11 +0800

    fixed domain name at site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,13 +1,12 @@
 export const siteConfig = {
-  name: "DKU Computer Science Club",
-  description:
-    "Website for the Duke Kunshan University Computer Science Club.",
-  author: "DKU Computer Science Club",
-  social: {
-    cs_club: "https://www.dkucompsci.org/",
-    instagram: "https://www.instagram.com/dkucompsciclub/",
-    linkedin: "https://www.linkedin.com/company/dkucompsciclub/",
-  },
+	name: "DKU Computer Science Club",
+	description: "Website for the Duke Kunshan University Computer Science Club.",
+	author: "DKU Computer Science Club",
+	social: {
+		cs_club: "https://dkucs.com/",
+		instagram: "https://www.instagram.com/dkucompsciclub/",
+		linkedin: "https://www.linkedin.com/company/dkucompsciclub/",
+	},
 };
 
 export type SiteConfig = typeof siteConfig;
